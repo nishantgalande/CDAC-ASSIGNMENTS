41024d7da46b188398022a24710acf50f1acb9f2
nishantgalande <nishant.galande.cmaug26@gmail.com>
2026-09-13 23:13:42 +0530

    Delete JAVA directory

--- JAVA/Assignment 1/src/Console.java
+++
@@ -1,8 +0,0 @@
-
-public class Console {
-	public static int getInt() throws Exception {
-		return System.in.read();
-	}
-}
-
-
--- JAVA/For Loop/src/EverySeventhNumber.java
+++
@@ -1,8 +0,0 @@
-public class EverySeventhNumber {
-	public static void main(String[] args) {
-
-		for (int iTemp = 7; iTemp <= 200; iTemp = iTemp + 7) {
-			System.out.println(iTemp);
-		}
-	}
-}
